21cf79458be7a85a811cadb792801bf920e3889b
Tomer Gibor <tomergibor@gmail.com>
2020-09-28 15:31:24 +0300

    Updated Makefiles, added Windows compiled version

---
+++ cmake-build-release-visual-studio/CMakeFiles/JSON_Parser.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Projects/C/JSON-Parser/file_operations.c" "C:/Projects/C/JSON-Parser/cmake-build-release-visual-studio/CMakeFiles/JSON_Parser.dir/file_operations.c.obj"
+  "C:/Projects/C/JSON-Parser/main.c" "C:/Projects/C/JSON-Parser/cmake-build-release-visual-studio/CMakeFiles/JSON_Parser.dir/main.c.obj"
+  "C:/Projects/C/JSON-Parser/parser.c" "C:/Projects/C/JSON-Parser/cmake-build-release-visual-studio/CMakeFiles/JSON_Parser.dir/parser.c.obj"
+  "C:/Projects/C/JSON-Parser/printers.c" "C:/Projects/C/JSON-Parser/cmake-build-release-visual-studio/CMakeFiles/JSON_Parser.dir/printers.c.obj"
+  "C:/Projects/C/JSON-Parser/utils.c" "C:/Projects/C/JSON-Parser/cmake-build-release-visual-studio/CMakeFiles/JSON_Parser.dir/utils.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-release-visual-studio/CMakeFiles/JSON_Parser.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JSON_Parser.dir/file_operations.c.obj"
+  "CMakeFiles/JSON_Parser.dir/main.c.obj"
+  "CMakeFiles/JSON_Parser.dir/parser.c.obj"
+  "CMakeFiles/JSON_Parser.dir/printers.c.obj"
+  "CMakeFiles/JSON_Parser.dir/utils.c.obj"
+  "JSON_Parser.exe"
+  "JSON_Parser.exe.manifest"
+  "JSON_Parser.lib"
+  "JSON_Parser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/JSON_Parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
